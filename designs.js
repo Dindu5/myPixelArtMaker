52de3d23fd6806fc428266962f8a7554d3262c38
Dindu5 <chimdindue@gmail.com>
2018-06-30 14:22:55 +0100

    Initial Commit

---
+++ designs.js
@@ -0,0 +1,16 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+const sizePicker, colorPicker
+
+sizePicker = $('#sizePicker');
+
+colorPicker = $('#colorPicker');
+
+gridHeight = $('#sizePicker').next().first()
+function makeGrid() {
+
+// Your code goes here!
+
+}
